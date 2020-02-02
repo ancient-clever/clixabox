6fc4665b0feae31ec53e1b0d95e4ee04e4a336c9
ancient-clever <ancient-clever@outlook.com>
2020-02-02 19:34:14 +0300

    drag n drop upload

--- pictorage/urls.py
+++ pictorage/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('storage.urls', namespace='storage')),
+    # path('', include('storage.urls', namespace='storage')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^photos/', include('storage.urls', namespace='photos')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- storage/urls.py
+++ storage/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home
+from django.conf.urls import url
+
+from . import views
 
 app_name = 'storage'
 
 urlpatterns = [
-    path('', home, name='home'),
+    url(r'^clear/$', views.clear_database, name='clear_database'),
+    url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
+    url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
+    url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
 ]
